92729abd953e6d7b905562daa63e3d8afaf0ca04
Randy <randyarbolaez0@gmail.com>
2020-11-19 10:09:55 -0500

    added height to dropDown so statistics text doesnt overlap with the picker

--- component/Dropdown.js
+++ component/Dropdown.js
@@ -40,7 +40,10 @@ const Dropdown = () => {
       <View style={styles.wrapper}>
         <Picker
           selectedValue={state}
-          style={{ height: 50, width: 100 }}
+          style={{
+            height: 50,
+            width: 100,
+          }}
           onValueChange={(itemValue, itemIndex) => {
             setCounty(Data[itemValue][0]);
             setState(itemValue);
@@ -76,6 +79,7 @@ const styles = StyleSheet.create({
     width: "80%",
   },
   wrapper: {
+    height: "50%",
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "baseline",
